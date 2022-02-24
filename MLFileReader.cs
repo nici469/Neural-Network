e9e74aaae091d36aec14c3c2e1eca36279d5f304
Joshua Uwuseba <joshuwus@gmail.com>
2022-02-24 11:40:42 +0100

    defined file format: defining fields

--- MLFileReader.cs
+++ MLFileReader.cs
@@ -2,33 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Counter_Console
 {
     public class MLFileReader
     {
+        
         /// <summary>
         /// The number of input variables
         /// </summary>
-        public int Ni;
+        private int Ni;
+
         /// <summary>
         /// The number of output variables
         /// </summary>
-        public int No;
+        private int No;
+
         /// <summary>
-        /// the number of timesteps to be stored in MLInput and TOutput, or the number of timesteps
-        /// to be processed
+        /// the full path of the file containing the data to be read for ML training
         /// </summary>
-        public int T;
+        private string filePath;
+
+        /// <summary>
+        /// an array to store the lines of string read from filePath
+        /// </summary>
+        string[] lines;
+
+        /// <summary>
+        /// used to store the position of the start signature of the first timestep in the filePath
+        /// </summary>
+        int startSignPos;
+
+        /// <summary>
+        /// used to store the position of the ending signature of the first timestep in the filePath
+        /// </summary>
+        int firstEndSignPos;
+
+        /// <summary>
+        /// a signature string that specifies the start of a new timestep
+        /// </summary>
+        string startSign;
+
+        /// <summary>
+        /// a signature string that specifies the end of a timestep
+        /// </summary>
+        string endSign;
+
         /// <summary>
         /// Jagged array containig the ML input data in format [Timestep] [dataId] or
         /// [T][Nc]
         /// </summary>
-        public double[][] MLInput;
+        private double[][] MLInput;
+
         /// <summary>
         /// Jagged array containig the ML input data in format [Timestep] [dataId] or
         /// [T][Ni]
         /// </summary>
-        public double[][] TOutput;
+        private double[][] TOutput;
+        
+        
+        public void Init()
+        {
+            
+
+        }
     }
 }
